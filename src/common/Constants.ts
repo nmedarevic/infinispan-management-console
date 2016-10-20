b3e18ab441bcd869df381a2aebc358d353ebc4f4
Ryan Emerson <remerson@redhat.com>
2016-10-20 10:10:07 +0100

    ISPN-6807: Customization of console features by configuration

---
+++ src/common/Constants.ts
@@ -0,0 +1,4 @@
+export const INFINISPAN: boolean = false;
+export const JDG: boolean = true;
+export const BRAND_IMAGE: string = "assets/images/brand.svg";
+export const BRAND_NAME: string = "Red Hat JBoss Data Grid Server Administration Console";
