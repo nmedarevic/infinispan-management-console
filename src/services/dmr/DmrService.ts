80efe81cb6debbe9c65a92dab9eef7377cbeb545
vblagoje <vblagoje@redhat.com>
2016-10-24 15:10:19 +0200

    Safer DmrService url handling

--- src/services/dmr/DmrService.ts
+++ src/services/dmr/DmrService.ts
@@ -3,7 +3,7 @@ import {IDmrRequest} from "./IDmrRequest";
 import {AuthenticationService} from "../authentication/AuthenticationService";
 import {ICredentials} from "../authentication/ICredentials";
 import {IDmrCompositeReq} from "./IDmrCompositeReq";
-import {isNotNullOrUndefined} from "../../common/utils/Utils";
+import {isNotNullOrUndefined, isNullOrUndefined} from "../../common/utils/Utils";
 
 const module: ng.IModule = App.module("managementConsole.services.dmr", []);
 
@@ -127,7 +127,7 @@ export class DmrService {
       config.timeout = 2000;
     }
     let deferred: ng.IDeferred<any> = this.$q.defer<any>();
-    this.urlUpload = this.urlUpload === undefined ? this.generateBaseUrl(this.authService.getCredentials(), true) : this.urlUpload;
+    this.urlUpload = isNullOrUndefined(this.urlUpload) ? this.generateBaseUrl(this.authService.getCredentials(), true) : this.urlUpload;
     // And we finally post it
     this.$http.post(this.urlUpload, data, config)
       .then(
@@ -136,6 +136,7 @@ export class DmrService {
           let msg: string = this.processDmrFailure(failure);
           console.log(msg);
           deferred.reject(msg);
+          this.urlUpload = null;
         });
     return deferred.promise;
   }
@@ -154,7 +155,7 @@ export class DmrService {
       config.timeout = 2000;
     }
     let deferred: ng.IDeferred<any> = this.$q.defer<any>();
-    this.url = this.url === undefined ? this.generateBaseUrl(this.authService.getCredentials(), false) : this.url;
+    this.url = isNullOrUndefined(this.url) ? this.generateBaseUrl(this.authService.getCredentials(), false) : this.url;
     // And we finally post it
     this.$http.post(this.url, data, config)
       .then(
@@ -163,6 +164,7 @@ export class DmrService {
           let msg: string = this.processDmrFailure(failure);
           console.log(msg);
           deferred.reject(msg);
+          this.url = null;
         });
     return deferred.promise;
   }
